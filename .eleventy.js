583c6268f15cd90590eba98bcda526482c660727
Dimdark23 <84768889+Dimdark23@users.noreply.github.com>
2024-05-27 05:22:59 +0000

    add vid

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     // Copy the `css` and `js` directories to the output
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("image");
+    eleventyConfig.addPassthroughCopy("vid");
     eleventyConfig.addPassthroughCopy("js");
   
     // Return the configuration object
